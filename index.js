8a2a4d04ea66f5f10ec8c5670f9874a8a4d0239d
badway <badwaym19@gmail.com>
2024-03-21 21:06:01 +0200

    file task 1 js

---
+++ index.js
@@ -0,0 +1,96 @@
+// var number = +prompt ("enter your number");
+// if(number < 0  ){
+// console.log(" this is negative number")
+// }
+// else if(number  > 0   ){
+//     console.log(" this is a postive number")  
+// }  
+//   task2
+// var number = +prompt("enter your number");
+//  switch(number%2){
+//   case 0:
+//     console.log("even")
+//     break;
+//     default:
+//       console.log("odd")
+//  }
+// var number = +prompt("enter your number");
+// if ( number=="%2"     ) {
+//   console.log("even");
+// } 
+//  else if(number  !="%2"  ){
+// console.log("odd");
+//  }
+// var number = +prompt('enter your number');
+// if(number %2 ===0){
+// console.log("even");
+// }else{
+//   console.log(' odd');
+// }
+
+// task3
+// var c=  20;
+// var f = 18;
+// if(c>=f){
+//     console.log(" c greater ")
+
+// } 
+// else{
+//     console.log( "f greater" )
+// }
+// var a,b;
+// a =prompt("value a");
+// b=prompt("value b") ;
+// if(a>b){
+//     console.log("a greater") 
+// }
+// else{
+//     console.log( "b greater" )
+// }
+// task 4
+ 
+
+// var number = +prompt ("enter the number ");
+//   if(number>=90 && number<=100 ){
+//  console.log(" A");
+//  }else if (number >= 80 && number < 90) {
+//     console.log(" B");
+//  } else if (number >=70   && number <  80) {
+//     console.log(" C");
+//  }else if (number >=60   && number < 70) {
+//     console.log("  D");
+//  }  else if (number >=0    && number <  60) {
+//     console.log("   F");
+//  }else{
+//     console.log(" Null");
+//  }  
+//  var age=+prompt("enter the number ");
+// //  var price=+prompt("enter the number");
+// if(age<12  ){         
+//     console.log("Price equal to 5")
+// }  
+//  else if(age<18 ){   
+// console.log(" Price equal to  10")
+//  } 
+//  else if(age< 60 ){  
+//   console.log("price equal to 20")
+//  }
+//  else if(age>60  ){  
+//   console.log("price equal to 15"   )
+//  }
+
+// function calculateTicketPrice(age) {
+//   if (age < 12) {
+//    console.log("Ticket price:$5") 
+//   } else if (age < 18) {
+//    console.log("Ticket price:$10 ") 
+//   } else if (age < 60) {
+//      console.log("Ticket price:$20")  
+//   } else {
+//    console.log("Ticket price:$15 ")  
+//   }
+// }
+// console.log(calculateTicketPrice(8));    
+// console.log(calculateTicketPrice(15));   
+// console.log(calculateTicketPrice(35));   
+// console.log(calculateTicketPrice(70));  
